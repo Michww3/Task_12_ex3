7cc4f18faab1e6a32642b882adfdfbdafe0ae96f
User <User@W11PC>
2024-06-17 15:29:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex mutex = new Mutex(false, "MyMutex", out bool isOne);
+
+        if (isOne)
+        {
+            Console.WriteLine("All good");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Application is already running");
+            Console.ReadLine();
+        }
+    }
+}
